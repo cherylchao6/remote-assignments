5b33bd9faf78f7ea59d9450341a1150b984daded
cherylchao <nmpyt21@gmail.com>
2021-03-05 12:41:03 +0800

    1

--- Week-1/Assignment2/js/DOM.js
+++ Week-1/Assignment2/js/DOM.js
@@ -8,16 +8,14 @@ welcomeText.addEventListener('click', () => {
  });
 
  //Show更多column
-
-
-function myFunction() {
-  let showMoreButton = document.getElementById('showmore');
-  let secondContentDiv = document.getElementById('secondContent');
-  if (secondContentDiv.style.display === "none") {
-    showMoreButton.textContent = "Show More";
-    secondContentDiv.style.display = "none";
-  } else {
-    secondContentDiv.style.display === "none";
-    showMoreButton.textContent = "Show Less";
-  }
-}
+ showMoreButton.addEventListener('click', () => {
+   let showMoreButton = document.getElementById('showmore');
+   let secondContentDiv = document.getElementById('secondContent');
+   if (secondContentDiv.style.display === "none") {
+     showMoreButton.textContent = "Show More";
+     secondContentDiv.style.display = "none";
+   } else {
+     secondContentDiv.style.display === "none";
+     showMoreButton.textContent = "Show Less";
+   }
+ });
