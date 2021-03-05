d2031a0d1ef9290b1fa448b55cdc48a97465f90a
cherylchao <nmpyt21@gmail.com>
2021-03-05 12:26:25 +0800

    1

--- Week-1/Assignment2/js/DOM.js
+++ Week-1/Assignment2/js/DOM.js
@@ -1,20 +1,20 @@
-//讓 Welcome 變成 Have a Good time!
+
 
 let welcomeText = document.getElementById('welcomeText');
-;
+let showMoreButton = document.getElementById('showmore');
+let secondContentDiv = document.getElementById('secondContent');
+//讓 Welcome 變成 Have a Good time!
 welcomeText.addEventListener('click', () => {
   welcomeText.textContent = "Have a good time!";
  });
 
  //Show更多column
-let showMoreButton = document.getElementById('showmore');
-let secondContentDiv = document.getElementById('secondContent');
-showMoreButton.addEventListener('click', () => {
-  if (secondContentDiv.style.display === "none") {
-    showMoreButton.textContent = "Show More";
-    secondContentDiv.style.display = "none";
-} else {
-    secondContentDiv.style.display === "none";
-    showMoreButton.textContent = "Show Less";
-  }
-  });
+ showMoreButton.addEventListener('click', () => {
+   if (secondContentDiv.style.display === "none") {
+     showMoreButton.textContent = "Show More";
+     secondContentDiv.style.display = "none";
+   } else {
+     secondContentDiv.style.display === "none";
+     showMoreButton.textContent = "Show Less";
+   }
+ });
